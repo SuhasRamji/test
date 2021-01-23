dcb8d4c34e8b196e44d9ed3077056241d6c69809
SuhasRamji <suhas.ramji@gmail.com>
2021-01-23 22:48:17 +0530

    Changed Databse URI

--- models.py
+++ models.py
@@ -9,7 +9,10 @@ import pymysql
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:qwerty@localhost/new_tenant?charset=utf8'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:qwerty@localhost/new_tenant?charset=utf8'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://suhas:qwerty@localhost:5432/Client1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://hvdqeccloviaqz:7570a39b5330237a905a25665d201d03ca25de356eff1522f94b50ced18bfa4c' \
+                                        '@ec2-54-85-13-135.compute-1.amazonaws.com:5432/d38blvmp22htul'
 db = SQLAlchemy(app)
 
 
